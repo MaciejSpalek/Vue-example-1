090383c1a0775723fd14ed93de76a3e7c011ac41
MaciejSpalek <spwrtt@gmail.com>
2021-01-11 11:41:09 +0100

    Add styles and remove function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <input type="text" placeholder="todo" v-model="newItem" />
-    <button @click="addItem">dsa</button>
-    <div v-for="item in items" v-bind:key="item.id">
+    <div class="item">
+      <p>Nowe todo: {{ JSON.stringify(items) }}</p>
+      <input type="text" placeholder="todo" v-model="newItem" />
+      <button @click="addItem">dsa</button>
+    </div>
+    <div class="item" v-bind:class="{
+      completed: item.completed
+    }" v-for="item in items" v-bind:key="item.id">
       <h2>{{ item.title }}</h2>
+      <button v-if="!item.completed" @click="removeItem(item.id)">remove</button>
     </div>
   </div>
 </template>
@@ -26,10 +32,29 @@ export default {
           id: this.itemCounter,
         });
       this.newItem && this.itemCounter++;
+      this.newItem = '';
     },
+    removeItem(id) {
+      console.log(this.items[id].id, id)
+      const index = this.items.findIndex(item => item.id === id)
+      this.items[index].completed = true;
+    }
   },
 };
 </script>
 
 <style>
+.item {
+  border: 1px solid #cdcdcd;
+  margin: 8px;
+  padding: 10px;
+}
+
+.completed {
+  opacity: 0.5;
+}
+
+.completed h2 {
+  text-decoration: line-through;
+}
 </style>
